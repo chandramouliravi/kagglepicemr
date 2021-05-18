99487d13177614dbbe53d4cfdbef45460b655087
chandra <34848267+chandramouliravi@users.noreply.github.com>
2021-05-18 20:52:55 +0530

    Add files via upload

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -2,11 +2,11 @@ from PIL import Image
 import pandas as pd
 df = pd.read_csv(r"/home/hadoop/raw_data/boneage-training-dataset.csv")
 
-for i in df.id:
-    s_path="//home//hadoop//raw_data//boneage-training-dataset//boneage-training-dataset//"+str(i)+'.png'
-    d_path="//home//hadoop//clean_data//boneage-training-dataset//"+str(i)+'.jpg'
-    im = Image.open(s_path)
-    im.save(d_path)
+#for i in df.id:
+#    s_path="//home//hadoop//raw_data//boneage-training-dataset//boneage-training-dataset//"+str(i)+'.png'
+#    d_path="//home//hadoop//clean_data//boneage-training-dataset//"+str(i)+'.jpg'
+#    im = Image.open(s_path)
+#    im.save(d_path)
 
 df = pd.read_csv(r"/home/hadoop/raw_data/boneage-test-dataset.csv")
 for i in df['Case ID']:
